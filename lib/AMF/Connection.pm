b78a07a1954b95fc92f20a4b4efed7e5d7348ab4
Alberto Attilio Reggiori <areggiori@cpan.org>
2010-10-20 18:07:12 +0200

    Reworked POD documentation - started to add new Storable::AMF 0.79 AMF Date support story and explained how simple AMF Object mappings can be done

--- lib/AMF/Connection.pm
+++ lib/AMF/Connection.pm
@@ -365,38 +365,49 @@ See the sample usage synopsis above to start using the module.
 
 =head1 DATE TYPE SUPPORT
 
-The current 0.71 version of Storable::AMF does not support the AMF0/AFM3 Date type, and everything is being passed as string to the server (E.g. "2010-09-22T02:34:00"). Or as number (double) if in timestamp form. We will be seeking to submit a patch for Storable::AMF.
+The latest 0.79 version of Storable::AMF added basic date support with the new_date() and perl_data() utilitiy functions. This is just great. Internally AMF Date Type needs timestamps in milliseconds since the epoch in UTC ("neutral") timezone, and since Storable::AMF version 0.79 the module shields the user from that by automatically converting Date values to Perl double (number) scalar values.
 
-This means, for example, that when you pass parameters to a call() you can not pass date parameters. And when results are returned dates are by default encoded as number (double) and most likely it is up to the client application to deparse the right object property/field value (E.g. convert "timestamp_something" => 1285253834000 to a proper DateTime object or something).
+Since 0.20 version of AMF::Connection we introduced the AMF::Connection::Date class to let a user to creat dates and send them to an AMF server correctly as AMF Date Type. This means, for example, that when you can now pass proper AMF Date Type parameters to a call(). For the results workflow see the ISSUES below.
 
-For passing Date paramters to the server, if the server is a Java/Blazeds one the following hack works:
+=head2 USING OLD Storable::AMF VERSIONS
 
- my $param1 = "foo"; # string
- my $timestamp_something = 1285253834000; # serialized as number
- my $dateParam = bless( {
-        year   => 2010,
-        month  => 9,
-        day    => 22,
-        hour   => 02,
-        minute => 34,
-        second => 00
-        }, 'java.util.Date' ); #assuming UTC timezone and server side can map 'java.util.Date' in - 'java.util.Calendar' should also work
+In older versions (pre 0.79) of Storable::AMF there was no support for the AMF0/AFM3 Date Type, and everything was being passed as string to the server (E.g. "2010-09-22T02:34:00"). Or as double (number) if the date was in timestamp seconds since the epoch. This meant that an AMF::Connection in order to send a date (E.g. as parameter) had to multiply (or divide) by 1000 the timestamp (E.g. for $perl_timestamp=time(); my $amf_timestamp = $perl_timestamp*1000 vs. my $perl_timestamp = $amf_timestamp/1000), plus rely on the AMF server to cast timestamp AMF Numbers to Dates (I.e. which seems to work on most AMF servers in Java which cast Number to java.util.Date() or similar).
 
- my $params = [
-	$param1,
-	$timestamp_something,
-	$dateParam
-	];
- 
- $client->call( ..., $params );
+=head2 ISSUES
 
-Where the timezone is assumed in UTC from server-sdie, and/or need to be converted beforehand.
+There is still an issue when arbitrary Date structures are returned from an AMF server to an AMF::Connection (E.g. as part of values of structured AMF Objects). In this case, the AMF::Connection does not try to reparse the Perl object structure returned by Storable::AMF (see thaw()) and simply serialise AMF Date Type as number (double) seconds since the epoch. This means that an AMF::Connection client application can not rely on those Date returned by the server as being instances of the AMF::Connection::Date class, but will be represented as normal Perl scalars representing a double (number) seconds since the epoch.
 
-For other Java to ActionScript type mappings see http://livedocs.adobe.com/blazeds/1/javadoc/flex/messaging/io/amf/ActionMessageOutput.html#writeObject(java.lang.Object)
+Future versions of AMF::Connection will address this shortcoming.
+
+=head1 MAPPING OBJECTS
+
+By combining the power of Perl bless {}, $package_name syntax and the Storable::AMF freeze()/thaw() interface in used by AMF::Connection, it is possible to pass arbitrary structured AMF Objects the corresponding AMF server can interpret. This is possible due a simple Perl object reference to an hash is serialised to an AMF Object and can be mapped back on the server side. 
+
+This means that an AMF::Connection application can wrap all ActionScript / Flex AMF Objects around Perl Objects and get them sent; and returned into a Perl object BLOB using the power of Storable::AMF freeze()/thaw().A
+
+For example to send a SearchQueryFx AMF Object to the server an AMF::Connection advanced search call(), the following code could be used:
+
+my $client = new AMF::Connection ( ... );
+
+# ... prepare parameters...
+
+my $searchAMFObject = bless( {
+                   'searchId' => $searchId,
+                   'startHit' => int($startHit),
+                   'searchString' => $searchString,
+                   'hitsPerPage' => ($hitsPerPage) ? int($hitsPerPage) : 20,
+                   'sortId' => $sortId,
+       }, 'com.mycompany.application.flex.data.SearchQueryFx');
+
+my $response = $client->call( "MySearchSevice.searchAdvanced", [ $searchAMFObject ] );
+
+#....
+
+For other Java to ActionScript type mappings possibilities see http://livedocs.adobe.com/blazeds/1/javadoc/flex/messaging/io/amf/ActionMessageOutput.html#writeObject(java.lang.Object)
 
 For PHP gateways at the moment there is not a known/documented way to map client to server objects.
 
-If the AMF gateway is using any Perl glue the DataTime perl object might be mapped in.
+Future versions of AMF::Connection may add a proper configurable factory for application specific ActionScript/Flex object mappings.
 
 =head1 METHODS
 
